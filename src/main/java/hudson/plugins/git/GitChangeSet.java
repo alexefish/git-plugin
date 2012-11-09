6a70cd65abf200504678a8f07b642579fff92170
Alex Fish <fish@ustwo.co.uk>
2012-11-09 12:21:53 +0000

    Updating author regex

--- src/main/java/hudson/plugins/git/GitChangeSet.java
+++ src/main/java/hudson/plugins/git/GitChangeSet.java
@@ -31,7 +31,7 @@ import java.util.regex.Pattern;
  */
 public class GitChangeSet extends ChangeLogSet.Entry {
 
-    private static final String PREFIX_AUTHOR = "author ";
+    private static final String PREFIX_AUTHOR = "Author ";
     private static final String PREFIX_COMMITTER = "committer ";
     private static final String IDENTITY = "(.*)<(.*)> (.*) (.*)";
     private static final String PREFIX_BRANCH = "Changes in branch ";
